67ecfb5a175846e9f84f02e2d957e7a890363a01
Nicole Reyes Oyarce <67403670+NicoleReyeso@users.noreply.github.com>
2021-07-08 15:55:25 -0400

    Add files via upload

---
+++ main_jquery.js
@@ -0,0 +1,45 @@
+$(".btn-vermas").click (
+    function () {
+        $(".btn-vermas").attr( "src" , "../vistas_bootstrap/producto1_foto2.jpg" )
+    }
+)
+
+
+
+
+$ (".btn-vermas2").click (
+        function () {
+        $(".btn-vermas2").attr( "src" , "../vistas_bootstrap/producto2_foto2.jpg")
+        
+        }
+            
+    
+)
+
+
+$ (".btn-vermas3").click (
+    function () {
+    $(".btn-vermas3").attr( "src" , "../vistas_bootstrap/producto3_foto2.jpg")
+    })
+  
+        
+
+
+$ (".btn-vermas4").click (
+    function () {
+    $(".btn-vermas4").attr( "src" , "../vistas_bootstrap/producto4_foto2.jpg" )
+    
+    }
+        
+
+)
+
+$ (".btn-vermas5").click (
+    function () {
+    $(".btn-vermas5").attr( "src" , "../vistas_bootstrap/producto5_foto2.jpg" )
+    
+    }
+        
+
+)
+
